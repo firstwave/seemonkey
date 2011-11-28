c85f59a11c97f6ff58a94b84c03d9af67e158409
Oliver <obartley@ebay.com>
2011-11-28 15:10:16 -0800

    cleaned up example distro package; better console info

--- example/build-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-jar cvfm ../bin/seemonkey.jar ../manifest.txt -C ../bin com/
-#jar cvfm ../bin/seemonkey.jar ../manifest.txt -C ../bin com/ -C .. libs/
-
-#monkeyrunner -plugin ../bin/seemonkey.jar monkeyrunner.py
