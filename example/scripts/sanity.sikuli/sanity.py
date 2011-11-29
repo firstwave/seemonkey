037d4a21d5cf3b653c9f6bfa027c794343ac5f60
Oliver <obartley@ebay.com>
2011-11-28 16:25:27 -0800

    stripped down sanity test to bare essentials

--- example/scripts/sanity.sikuli/sanity.py
+++ example/scripts/sanity.sikuli/sanity.py
@@ -4,19 +4,14 @@ import sys
 
 
 from com.criticalpath.seemonkey import SeeMonkey
-#from com.android.monkeyrunner import MonkeyDevice
-from org.sikuli.script import Settings
 
-print "set up android connection"
-scr = SeeMonkey() # compatiable with Sikuli Screen/Region
+scr = SeeMonkey()
 
 assert scr != None
-dev = scr.getMonkeyDevice() # Android Monkey device
+
 scr.autoDelay = 100
 class SeeMonkeyTest(unittest.TestCase):
     def testSanity(self):
-        print("This is an example of output on STDOUT")
-        sys.stderr.write("This is an example of output on STDERR")
         scr.wake()
         scr.press('HOME')
      
