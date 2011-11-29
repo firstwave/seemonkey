7644486c8bc0960ff7d32dfd323cf2356485b048
Oliver <obartley@ebay.com>
2011-11-28 16:21:30 -0800

    cleaned up setup code

--- example/scripts/sanity.sikuli/sanity.py
+++ example/scripts/sanity.sikuli/sanity.py
@@ -4,11 +4,8 @@ import sys
 
 
 from com.criticalpath.seemonkey import SeeMonkey
-from com.android.monkeyrunner import MonkeyDevice
+#from com.android.monkeyrunner import MonkeyDevice
 from org.sikuli.script import Settings
-
-Settings.MoveMouseDelay = 0.01
-Settings.AutoWaitTimeout = 10
 
 print "set up android connection"
 scr = SeeMonkey() # compatiable with Sikuli Screen/Region
