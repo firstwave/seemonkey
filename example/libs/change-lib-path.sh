f7e18faaed15a219f83ab9ba6d3647074a76f341
Oliver <obartley@ebay.com>
2011-12-05 10:29:14 -0800

    separate dist repo

--- example/libs/change-lib-path.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for dylib in *.dylib *.jnilib
-do
-   for ref in `otool -L $dylib | grep "lib.*.dylib[^:]" | awk '{print $1'} | grep -v '^/usr/lib' | grep -v '^/System/'`
-   do
-      #echo $ref
-      install_name_tool -change $ref @loader_path/`basename $ref` $dylib
-   done
-done
--- example/scripts/testmodule.sikuli/testmodule.py
+++
@@ -1,3 +0,0 @@
-from sikuli import *
-
-print(".sikuli loaded")
