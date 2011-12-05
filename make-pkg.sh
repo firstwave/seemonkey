6cbb3bf82e5965187d0ee015f45db0ff7732f802
Oliver <obartley@ebay.com>
2011-12-05 12:23:12 -0800

    v1.2

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-DISTRIBUTION_DIR="$HOME/Projects/SeeMonkeyDist"
 jar cvfm bin/seemonkey.jar manifest.txt -C bin com/
 echo "copying .jar to distribution package"
-cp bin/seemonkey.jar "$DISTRIBUTION_DIR/libs/"
+cp bin/seemonkey.jar /libs/
